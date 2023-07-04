6c7d94dda76924dfb8d582925046461e5892be78
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-07-04 08:34:08 +0200

    imported bootstap

--- index.php
+++ index.php
@@ -46,26 +46,35 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <title>Hotels</title>
     </head>
     <body>
          <?php foreach ($hotels as $hotel) { ?>
-           <div>
-                <h1>
-                    <?php echo $hotel['name'];?>
-                </h1>
-                <h2>
-                    <?php echo $hotel['description'];?>
-                </h2>
-                <span>
-                    <?php echo $hotel['parking'];?>
-                </span>
-                <span>
-                    <?php echo $hotel['vote'];?>
-                </span>
-                <span>
-                    <?php echo $hotel['distance_to_center'];?>
-                </span>
+            
+           <div class="container d-flex justify-content-center align-content-between">
+                <div class="h_title">
+                    <h1>
+                        <?php echo $hotel['name'];?>
+                    </h1>
+                </div>
+                <div class="h_description">
+                    <h2>
+                        <?php echo $hotel['description'];?>
+                    </h2>
+                </div>
+                <div class="addictional">
+                    <span>
+                        <?php echo $hotel['parking'];?>
+                    </span>
+                    <span>
+                        <?php echo $hotel['vote'];?>
+                    </span>
+                    <span>
+                        <?php echo $hotel['distance_to_center'];?>
+                    </span>
+                </div>
            </div> 
         <?php } ?>
     </body>
